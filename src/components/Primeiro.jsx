dee8f961ef45a8aea7c42e98a0ab0a1e46e6dde8
Giovani da Silva <giovani-vani@hotmail.com>
2021-05-01 11:36:37 -0300

    Primeiro Componente

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+// function Primeiro() {
+//   return (
+//     // <div>
+//     //   <h1>Primeiro</h1>
+//     //   <h2>Exemplo de componente React</h2>
+//     // </div>
+
+//     //Retornar dois elementos adjacentes sem a tag div
+//     <>
+//       <h1>Primeiro</h1>
+//       <h2>Exemplo de componente React</h2>
+//     </>
+
+//     //Retornar dois elementos adjacentes sem a tag div
+//     // <React.Fragment>
+//     //     <h1>Primeiro</h1>
+//     //     <h2>Exemplo de componente React</h2>
+//     // </React.Fragment>
+//   );
+// }
+
+// export default Primeiro;
+
+//Reduzir o maximo de sintaxe transformando em funcao aron
+export default () => 
+        <>
+          <h1>Primeiro</h1>
+          <h2>Exemplo de componente React</h2>
+        </>
+
+ 
+     
+    
+    
