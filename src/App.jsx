01be853c433cdead25f084a5f382360ab4efb837
Giovani da Silva <giovani-vani@hotmail.com>
2021-05-01 12:53:46 -0300

    Melhorando o layout dos Cards

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './App.css'
+import Card from './components/layout/Card'
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
+
+export default props => (
+  <div className="App">
+    <Card titulo="03- Componente com Filhos">
+      <ComFilhos>
+        <ul>
+          <li>Ana</li>
+          <li>Bia</li>
+          <li>Carlos</li>
+          <li>Daniel</li>
+        </ul>
+      </ComFilhos>
+    </Card>
+
+    <Card titulo="02 - Componente com Parametros">
+      <ComParametro
+        titulo="Esse é o titulo"
+        subtitulo="Esse é o subtitulo"
+      ></ComParametro>
+    </Card>
+
+    <Card titulo="01 - Primeiro Componente">
+      <Primeiro></Primeiro>
+    </Card>
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -1,39 +1,9 @@
 import React from 'react'
 import reactDom from 'react-dom'
 import './index.css'
-import Card from './components/layout/Card'
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
-import ComFilhos from './components/ComFilhos'
+import App from './App'
 
 reactDom.render(
-    <div>
-
-        <Card titulo="03- Componente com Filhos">
-            <ComFilhos>
-                <ul>
-                    <li>Ana</li>
-                    <li>Bia</li>
-                    <li>Carlos</li>
-                    <li>Daniel</li>
-                </ul>
-            </ComFilhos>
-        </Card>
-
-        <Card titulo="02 - Componente com Parametros">
-            <ComParametro titulo='Esse é o titulo'
-                subtitulo='Esse é o subtitulo'></ComParametro>
-        </Card>
-
-        <Card titulo="01 - Primeiro Componente">
-            <Primeiro ></Primeiro>
-        </Card>
-
-
-
-
-
-
-    </div>,
+    <App></App>,
     document.getElementById('root')
 )
