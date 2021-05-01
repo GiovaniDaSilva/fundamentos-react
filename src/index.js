7e4a3e18e1904c7418c37af1d932b80e9cd2b330
Giovani da Silva <giovani-vani@hotmail.com>
2021-05-01 12:17:16 -0300

    Componente com Parametros

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import reactDom from 'react-dom'
-import  './index.css'
+import './index.css'
 import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
 
 reactDom.render(
-    
-    <Primeiro></Primeiro>,
+    <div>
+        <Primeiro ></Primeiro>
+
+        <ComParametro titulo='Esse é o titulo'
+            subtitulo='Esse é o subtitulo'></ComParametro>
+
+    </div>,
     document.getElementById('root')
 )
