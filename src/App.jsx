82f689c22c35606cc0ba3b88f9e9dbb63a5c5c41
Giovani da Silva <giovani-vani@hotmail.com>
2021-05-01 13:15:47 -0300

    Componentes de Repeticao

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-import './App.css'
-import Card from './components/layout/Card'
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
-import ComFilhos from './components/ComFilhos'
-import Repeticao from './components/Repeticao'
+import "./App.css";
+import Card from "./components/layout/Card";
+import Primeiro from "./components/Primeiro";
+import ComParametro from "./components/ComParametro";
+import ComFilhos from "./components/ComFilhos";
+import Repeticao from "./components/Repeticao";
+import Condicional from "./components/Condicional";
+import CondicionalComIf from './components/CondicionalComIf'
 
-export default props => (
+export default (props) => (
   <div className="App">
+   <Card titulo="06 - Condicional v2">
+      <CondicionalComIf numero={10}></CondicionalComIf>
+    </Card>
 
-    <Card titulo="04 - Repetição v1">
-        <Repeticao></Repeticao>
+    <Card titulo="05 - Condicional v1">
+      <Condicional numero={11}></Condicional>
+    </Card>
+
+    <Card titulo="05 - Condicional v1">
+      <Condicional numero={11}></Condicional>
+    </Card>
+
+    <Card titulo="04 - Repetição">
+      <Repeticao></Repeticao>
     </Card>
 
     <Card titulo="03- Componente com Filhos">
